f02a4f3f265a0bf89ee3e5f0b2b0449b0e81f8f2
Madhur Grover <grovermadhur@me.com>
2025-02-13 13:45:02 -0800

    Initial Commit

---
+++ Hanuman Chalisa Kids/Hanuman_Chalisa_KidsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hanuman_Chalisa_KidsApp.swift
+//  Hanuman Chalisa Kids
+//
+//  Created by Madhur Grover on 2/13/25.
+//
+
+import SwiftUI
+
+@main
+struct Hanuman_Chalisa_KidsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Hanuman Chalisa KidsTests/Hanuman_Chalisa_KidsTests.swift
@@ -0,0 +1,17 @@
+//
+//  Hanuman_Chalisa_KidsTests.swift
+//  Hanuman Chalisa KidsTests
+//
+//  Created by Madhur Grover on 2/13/25.
+//
+
+import Testing
+@testable import Hanuman_Chalisa_Kids
+
+struct Hanuman_Chalisa_KidsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
